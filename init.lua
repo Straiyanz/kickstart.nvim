1bcccbc37865c9ac38ff78712b9b3a5f5a608e51
luke <luke.eckersley@gmail.com>
2025-08-17 00:05:39 +0800

    Added Harpoon, floating temrinal

--- init.lua
+++ init.lua
@@ -247,7 +247,16 @@ rtp:prepend(lazypath)
 -- NOTE: Here is where you install your plugins.
 require('lazy').setup({
   -- NOTE: Plugins can be added with a link (or for a github repo: 'owner/repo' link).
-  'NMAC427/guess-indent.nvim', -- Detect tabstop and shiftwidth automatically
+  {
+    'NMAC427/guess-indent.nvim', -- Detect tabstop and shiftwidth automatically
+    config = function()
+      require('guess-indent').setup {
+        on_tab_options = {
+          ['expandtabs'] = true,
+        },
+      }
+    end,
+  },
 
   -- NOTE: Plugins can also be added by using a table,
   -- with the first argument being the link and the following
@@ -985,15 +994,18 @@ require('lazy').setup({
   -- require 'kickstart.plugins.debug',
   -- require 'kickstart.plugins.indent_line',
   -- require 'kickstart.plugins.lint',
-  -- require 'kickstart.plugins.autopairs',
-  -- require 'kickstart.plugins.neo-tree',
+  require 'kickstart.plugins.autopairs',
+  require 'kickstart.plugins.neo-tree',
   -- require 'kickstart.plugins.gitsigns', -- adds gitsigns recommend keymaps
 
   -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
   --    This is the easiest way to modularize your config.
   --
   --  Uncomment the following line and add your plugins to `lua/custom/plugins/*.lua` to get going.
-  -- { import = 'custom.plugins' },
+  --  Currently uncludes:
+  --    Harpoon2
+  --    Custom Floating Terminal, thanks TJ!
+  { import = 'custom.plugins' },
   --
   -- For additional information with loading, sourcing and examples see `:help lazy.nvim-🔌-plugin-spec`
   -- Or use telescope!
---
+++ lua/custom/plugins/harpoon.lua
@@ -0,0 +1,37 @@
+return {
+  'ThePrimeagen/harpoon',
+  branch = 'harpoon2',
+  config = function()
+    local harpoon = require 'harpoon'
+    ---@diagnostic disable-next-line: missing-parameter
+    harpoon:setup()
+
+    local function map(lhs, rhs, opts)
+      vim.keymap.set('n', lhs, rhs, opts or {})
+    end
+
+    map('<leader>a', function()
+      harpoon:list():add()
+    end, { desc = 'Harpoon add to list' })
+
+    map('<C-e>', function()
+      harpoon.ui:toggle_quick_menu(harpoon:list())
+    end, { desc = 'Harpoon toggle menu' })
+
+    map('<leader>q', function()
+      harpoon:list():select(1)
+    end, { desc = 'Harpoon jump to 1' })
+
+    map('<leader>w', function()
+      harpoon:list():select(2)
+    end, { desc = 'Harpoon jump to 2' })
+
+    map('<leader>e', function()
+      harpoon:list():select(3)
+    end, { desc = 'Harpoon jump to 3' })
+
+    map('<leader>r', function()
+      harpoon:list():select(4)
+    end, { desc = 'Harpoon jump to 4' })
+  end,
+}
