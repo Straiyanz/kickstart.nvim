82d37d54eea493135f8ba24da854be6c9349c89d
luke <luke.eckersley@gmail.com>
2025-08-17 00:58:49 +0800

    Fix harpoon error

--- lua/custom/plugins/harpoon.lua
+++ lua/custom/plugins/harpoon.lua
@@ -2,38 +2,51 @@ return {
   'ThePrimeagen/harpoon',
   branch = 'harpoon2',
   dependencies = { 'nvim-lua/plenary.nvim' },
+  keys = {
+    {
+      '<C-e>',
+      function()
+        require('harpoon').ui:toggle_quick_menu(require('harpoon'):list())
+      end,
+    },
+    {
+      '<leader>a',
+      function()
+        require('harpoon'):list():add()
+      end,
+      desc = 'Harpoon add to list',
+    },
+    {
+      '<leader>1',
+      function()
+        require('harpoon'):list():select(1)
+      end,
+      desc = 'Harpoon 1',
+    },
+    {
+      '<leader>2',
+      function()
+        require('harpoon'):list():select(2)
+      end,
+      desc = 'Harpoon 2',
+    },
+    {
+      '<leader>3',
+      function()
+        require('harpoon'):list():select(3)
+      end,
+      desc = 'Harpoon 3',
+    },
+    {
+      '<leader>4',
+      function()
+        require('harpoon'):list():select(4)
+      end,
+      desc = 'Harpoon 4',
+    },
+  },
   config = function()
-    local harpoon = require 'harpoon'
-    ---@diagnostic disable-next-line: missing-parameter
-    harpoon:setup()
+    require('harpoon'):setup()
     require('telescope').load_extension 'harpoon'
-
-    local function map(lhs, rhs, opts)
-      vim.keymap.set('n', lhs, rhs, opts or {})
-    end
-
-    map('<leader>a', function()
-      harpoon:list():add()
-    end, { desc = 'Harpoon add to list' })
-
-    map('<C-e>', function()
-      harpoon.ui:toggle_quick_menu(harpoon:list())
-    end, { desc = 'Harpoon toggle menu' })
-
-    map('<leader>q', function()
-      harpoon:list():select(1)
-    end, { desc = 'Harpoon jump to 1' })
-
-    map('<leader>w', function()
-      harpoon:list():select(2)
-    end, { desc = 'Harpoon jump to 2' })
-
-    map('<leader>e', function()
-      harpoon:list():select(3)
-    end, { desc = 'Harpoon jump to 3' })
-
-    map('<leader>r', function()
-      harpoon:list():select(4)
-    end, { desc = 'Harpoon jump to 4' })
   end,
 }
