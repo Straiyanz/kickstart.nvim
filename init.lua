07ac962a43cfdce6d183bb1b5ed44bf40ff602bf
luke <luke.eckersley@gmail.com>
2025-10-22 18:06:35 +0800

    Open Diagnostic
    Bound to <leader>d (<space>d)

--- init.lua
+++ init.lua
@@ -211,6 +211,11 @@ vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper win
 -- vim.keymap.set("n", "<C-S-j>", "<C-w>J", { desc = "Move window to the lower" })
 -- vim.keymap.set("n", "<C-S-k>", "<C-w>K", { desc = "Move window to the upper" })
 
+-- Diagnostics
+vim.keymap.set('n', '<leader>d', function()
+  vim.diagnostic.open_float(nil, { focus = true, scope = 'line' })
+end, { desc = 'Open Diagnostic' })
+
 -- [[ Basic Autocommands ]]
 --  See `:help lua-guide-autocommands`
 
